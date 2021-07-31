24fcba4cc936edf249d5443f355a8e0548bb0cc4
deokgoo <kkddgg1001@gmail.com>
2021-08-01 00:04:04 +0900

    add google analytics trace id (#3)

--- nuxt.config.js
+++ nuxt.config.js
@@ -35,7 +35,11 @@ export default {
     // https://go.nuxtjs.dev/typescript
     '@nuxt/typescript-build',
     // https://go.nuxtjs.dev/stylelint
-    '@nuxtjs/stylelint-module'
+    '@nuxtjs/stylelint-module',
+    // https://www.npmjs.com/package/@nuxtjs/google-analytics
+    '@nuxtjs/google-analytics', {
+      id: 'UA-136405885-3'
+    }
   ],
 
   // Modules: https://go.nuxtjs.dev/config-modules
